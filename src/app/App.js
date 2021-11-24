a60c9cab0700d330bac8229d06af9d4df8345654
fedya <feodosiyshapovalov@gmail.com>
2021-11-24 16:34:32 +0200

    routing_lessons

---
+++ src/app/App.js
@@ -0,0 +1,28 @@
+import NavBar from "./components/navBar";
+import { Switch, Route, Redirect } from "react-router-dom"
+import Dashboard from "./components/dashboard";
+import Home from "./components/home";
+import Posts from "./components/posts";
+import Login from "./components/login";
+import NotFound from "./components/not-found";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>App</h1>
+      <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/posts/:postId?" component={Posts}/>
+          <Route path="/404" component={NotFound} />
+          <Redirect from="/admin" to="/dashboard" />
+          <Redirect to="/404" />
+      </Switch>
+     
+    </div>
+  );
+}
+
+export default App;
